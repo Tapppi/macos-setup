02acc8c2d7c6441012de06cde97f50c993d09c7c
Tapani Moilanen <moilanen.tapani@gmail.com>
2019-02-19 15:56:40 +0200

    Backup restore and file list update

--- restore.sh
+++ restore.sh
@@ -4,15 +4,16 @@ backup_path="${backup_path:=$1}"
 : "${backup_path:?Missing backup_path, provide as argument (relative to home dir)}"
 d=$(dirname "$0")
 files_from=$(greadlink -f "$d")/restore.bom
+backup_path_resolved=$(greadlink -f "$d")/restore.bom
 
 shift
 
-# Run in home folder
-cd || exit;
-
 function doIt() {
-	echo "Backing up $(pwd) to ${backup_path} as a gzipped tar ball...";
+	echo "Restoring from ${backup_path_resolved} to ~...";
 	echo "";
+
+	mkdir .tmp-backup-dir
+	cd .tmp-backup-dir && tar -xzf ${backup_path_resolved};
 	rsync \
     --rsync-path="sudo rsync" \
     --perms \
@@ -22,21 +23,16 @@ function doIt() {
     --links \
     --hard-links \
     --files-from="$files_from" \
-		-avh . ~/.tmp-backup-dir;
-	cd ~/.tmp-backup-dir && tar -czf ../.tmp-backup-dir.tgz . && cd -;
+		-avh . ~;
+  cd - && rm -rf .tmp-backup-dir;
 
-	cd "$(dirname "${BASH_SOURCE}")";
-  mv ~/.tmp-backup-dir.tgz $backup_path;
-	cd -;
-
-	echo "Backed up successfully";
-	rm -rf ~/.tmp-backup-dir;
+	echo "Restored";
 }
 
 if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
 	doIt;
 else
-	read -rp "Do you want to backup $(pwd) to ${backup_path}? (y/n) " -n 1;
+	read -rp "Do you want to restore ${backup_path} to ~? (y/n) " -n 1;
 	echo "";
 	if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 		doIt;
