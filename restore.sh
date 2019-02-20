34530a3c92f61c7b010d84d7b6344f921e728b7d
Tapani Moilanen <moilanen.tapani@gmail.com>
2019-02-21 00:36:05 +0200

    Fix restore.sh

--- restore.sh
+++ restore.sh
@@ -4,7 +4,7 @@ backup_path="${backup_path:=$1}"
 : "${backup_path:?Missing backup_path, provide as argument (relative to home dir)}"
 d=$(dirname "$0")
 files_from=$(greadlink -f "$d")/restore.bom
-backup_path_resolved=$(greadlink -f "$d")/restore.bom
+backup_path_resolved=$(greadlink -f "$backup_path")
 
 shift
 
@@ -39,4 +39,3 @@ else
 	fi;
 fi;
 unset doIt;
-cd -;
