c5eb25705a00cbfea8199860094484b9b7233ef9
Tapani Moilanen <moilanen.tapani@gmail.com>
2019-02-21 19:42:11 +0200

    Add energy saver settings in config task

--- tasks/config.sh
+++ tasks/config.sh
@@ -16,6 +16,7 @@ config () {
 
   p1 "Customising various launch options"
   custom_loginitems
+  config_energy
   custom_terminal
   custom_duti
 
@@ -79,6 +80,32 @@ config_launchd () {
   config_plist "$2" "$1" "$4" "$3" && \
     $3 plutil -convert xml1 "$1" && \
     $3 launchctl load "$1"
+}
+
+# Configure Energy Saver
+
+_energy='-b displaysleep	20
+-b	sleep	90
+-b	disksleep	45
+-b	womp	1
+-b	powernap	1
+-c	displaysleep	60
+-c	sleep	0
+-c	disksleep	90
+-c	womp	1
+-c	autorestart	1
+-c	powernap	1
+-u	displaysleep	2
+-u	lessbright	1
+-u	haltafter	60
+-u	haltremain	5
+-u	haltlevel	5'
+
+config_energy () {
+  printf "%s\n" "${_energy}" | \
+  while IFS="$(printf '\t')" read flag setting value; do
+    sudo pmset $flag ${setting} ${value}
+  done
 }
 
 # Define Function =config_xcode=
