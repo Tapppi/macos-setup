fb471a239484432537d9d0a582f827ee3606a69b
Tapani Moilanen <moilanen.tapani@gmail.com>
2024-02-22 03:06:56 +0200

    Remove unused tools and configurations

--- tasks/install.sh
+++ tasks/install.sh
@@ -8,8 +8,6 @@ sudo -v
 install () {
   # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-  
-  init_no_sleep
 
   install_macos_sw
   install_node_sw
